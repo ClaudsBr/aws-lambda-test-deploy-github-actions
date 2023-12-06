0e4bd0102859f390ff3129509c33a5fb63860913
ClaudsBr <clauds.true@gmail.com>
2023-12-06 11:37:42 -0300

    second commit

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,7 +2,7 @@ from log import log
 import json
 
 def lambda_handler(event, context):   
-    log(f"Log de execução do evento: {json.dumps(event)} ") 
+    log(f"Log de execução do evento após o GitHub Actions: {json.dumps(event)} ") 
     return {
         'statusCode': 200,
         'body': json.dumps(event)
