aa93054baf808f0611720a0c49bc21783aeb56b7
ClaudsBr <clauds.true@gmail.com>
2023-12-06 14:57:18 -0300

    Converting my function in an API

--- my_lambda/lambda_function.py
+++ my_lambda/lambda_function.py
@@ -5,5 +5,8 @@ def lambda_handler(event, context):
     log(f"Log de execução do evento após o GitHub Actions: {json.dumps(event)} ") 
     return {
         'statusCode': 200,
-        'body': json.dumps(event)
+        'body': f"<html><body>Dados da Requisicao: {json.dumps(event)}</body></html>",
+        'headers': {
+            "content-type": "text/html"
+        }
     }
